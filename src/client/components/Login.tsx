9196da1b2675fa376bdc0cbc83425205035b0dde
shamin <shamin99@outlook.com>
2019-11-29 12:19:08 -0700

    tweaks

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -5,13 +5,13 @@ import styled from 'styled-components';
 import ActiveLogin from '../ActiveLogin';
 import { useHistory } from "react-router-dom";
 
-const ButtonDiv = styled.div`     
+const ButtonDiv:any = styled.div`     
     display:flex;
     justify-content: center;
     align-content: center;
     padding-top: 10px;
 `
-const FormDiv = styled.div`     
+const FormDiv:any = styled.div`     
     margin: auto;
     width: 300px;
     background-color: rgb(230, 230, 230);
@@ -30,37 +30,6 @@ class Login extends React.Component{
         username: '',
         password: '',
         type: ''
-    }
-    getAllRecords = () => {
-            console.log(this.state.type);
-            this.find();
-    }
-
-    async getAllRecord(){
-        try {
-			let r = await fetch('/api/healthrecords');
-			let healthrecords = await r.json();
-            console.log(healthrecords);
-		} catch (error) {
-			console.log(error);
-		}
-    }
-
-    async find() {
-        try {
-            let r = await fetch('/api/healthrecords',{          //JSON.stringify({username: 'rahman', password: '8002'})
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(this.state)
-            });
-            let result = await r.json();
-            console.log(result);
-
-        } catch (error) {
-            console.log(error);
-        }
     }
 
     dropListener = (e: any) => {
@@ -163,8 +132,6 @@ class Login extends React.Component{
 
                     <ButtonDiv>
                         <Button variant="primary" onClick = {this.action} >Sign in</Button>
-                        <Button variant="primary" onClick = {this.action} >Records</Button>
-                        
                     </ButtonDiv>
 
                     </Form>
--- src/client/components/userPage.tsx
+++ src/client/components/userPage.tsx
@@ -5,6 +5,42 @@ import ActiveLogin from '../ActiveLogin';
 
 class UserPage extends React.Component{
 
+    state = {
+        username: '',
+        password: '',
+        type: ''
+    }
+
+    getAllRecords = () => {
+        console.log(this.state.type);
+        this.find();
+    }
+    async getAllRecord(){
+        try {
+            let r = await fetch('/api/healthrecords');
+            let healthrecords = await r.json();
+            console.log(healthrecords);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async find() {
+        try {
+            let r = await fetch('/api/healthrecords',{          //JSON.stringify({username: 'rahman', password: '8002'})
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.state)
+            });
+            let result = await r.json();
+            console.log(result);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     render(){
         return(
 
