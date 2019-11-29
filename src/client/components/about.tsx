11f168d9b262e178f5e4bfb9321b8f11492e890b
avneetgill <avneet2.gill@gmail.com>
2019-11-28 17:07:21 -0700

    worked on about page

--- src/client/components/about.tsx
+++ src/client/components/about.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
+//import { Jumbotron as Jumbo, Container} from 'react-bootstrap';
+ import Image from 'react-bootstrap/Image'
+
+// import Image from 'react-native'
+import { Component } from 'react';
+import { Jumbotron as Jumbo, Container, Row, Col} from 'react-bootstrap'
+import styled from 'styled-components'
+import image from './pharmacare.png';
+import imgMAKEwORK from "./../../../webpack.config"
+
+const ImgConst = styled.img` 
+    object-fit: none;
+    object-position: center;
+    width: 100%;
+    max-height: 100px;
+    margin-bottom: 1rem;
+    margin-top: 2px;
+`
+
 
 class About extends React.Component{
+    state = {
+    
+       
+    };
+     
     render() {
         return(
-            <div><h1>This is a project made by Shamin Rahman</h1></div>
+            <div>
+               <ImgConst src="https://images.pexels.com/photos/208512/pexels-photo-208512.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=350&w=1000" ></img>
+               
+            <h1>PharmaCare</h1>
+            <h2> shamin betcg hehe</h2>
+            </div>
         )
     }
 }
 
 export default About;
+
