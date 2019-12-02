ccf2e73926caa817931a0b7996aeb149a118a636
avneetgill <avneet2.gill@gmail.com>
2019-12-01 23:48:34 -0700

    some server stuff for client

---
+++ src/server/database/orders.ts
@@ -0,0 +1,37 @@
+import {Connection} from './index';
+
+export const listAllOrders = async () => {
+    return new Promise((resolve, reject) => {
+        Connection.query('SELECT * FROM orders', (err, result) => {
+            if(err){
+                console.log('error in query');
+                return reject(err);
+            }
+            else
+                resolve(result);
+        });
+    });
+}
+export const findClientOrders = async (req: any) => {
+
+    const username = req.username;
+ 
+    const query = 'SELECT * from orders WHERE clientID = ?';
+    const args = [username];
+
+    return new Promise((resolve, reject) => {
+        Connection.query(query, args, (err, result) => {
+            if(err){
+                return reject(err);
+            }
+            else{
+                resolve(result);
+            }
+        });
+    });
+}
+
+export default {
+   listAllOrders,
+   findClientOrders
+}
